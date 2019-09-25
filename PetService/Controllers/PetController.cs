c7803e27a585e466413c069e2e3949863436b5a5
Jeremy <jtroshynski@gmail.com>
2019-09-24 21:11:59 -0500

    Add project files.

---
+++ PetService/Controllers/PetController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using PetService.Models;
+
+namespace PetService.Controllers
+{
+    public class PetController : ApiController
+    {
+        Pet[] pets = new Pet[]
+        {
+            new Pet { id = 1, name = "Baba", species = "Rabbit", age = 1, sex = "M"},
+            new Pet { id = 2, name = "Petunia", species = "Cat", age = 1, sex = "F" },
+            new Pet { id = 3, name = "Mongo", species = "Dog", age = 1, sex = "M" }
+        };
+
+        public IEnumerable<Pet> GetAllPets()
+        {
+            return pets;
+        }
+
+        public IHttpActionResult GetPet(int id)
+        {
+            var pet = pets.FirstOrDefault((p) => p.id == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            return Ok(pet);
+        }
+    }
+}
---
+++ PetService/Models/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetService.Models
+{
+    public class Pet
+    {
+        //species of pet, it’s name, age, sex, description, pet owner’s contact info, and an image URL
+        public int id;
+        public string name;
+        public string species;
+        public int age;
+        public string sex;
+        public string description;
+        public string ownerEmail;
+        public string ownerPrimaryPhone;
+        public string imageURL;
+        
+    }
+}
